5e196aa30d0bfcbd0d1f5840a24d8e0c28890ebd
Li <liyunwei10@gmail.com>
2020-08-10 17:54:11 -0400

    fetch tweets from magic well v0.01

--- pages/results.js
+++ pages/results.js
@@ -1,8 +1,25 @@
 import React from "react";
+import fetch from "isomorphic-unfetch";
 import { withRouter } from "next/router";
+import useSWR from "swr";
+
+async function fetcher(url) {
+  const res = await fetch(url);
+  const json = await res.json();
+
+  return json;
+}
 
 const Results = (props) => {
-  return <div className="results">{props.router.query.keywords}</div>;
+  const { data, error } = useSWR(
+    `http://127.0.0.1:5000/tweets/search?keywords=${props.router.query.keywords}`,
+    fetcher
+  );
+
+  if (error) return <div className="results">failed to load</div>;
+  if (!data) return <div className="results">loading...</div>;
+
+  return <div className="results">{data[0]}</div>;
 };
 
 export default withRouter(Results);
