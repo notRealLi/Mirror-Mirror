589efbbeeee495556fe838ec11621cec8e86f677
Li <liyunwei10@gmail.com>
2020-08-13 19:31:47 -0400

    set image opacity to 0 when submit button is disabled

--- pages/index.js
+++ pages/index.js
@@ -65,18 +65,7 @@ const Index = () => {
         />
       </label>
       <button onClick={showResults} disabled={keywords === ""}>
-        <motion.img
-          src="/eye.svg"
-          alt=""
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1,
-            transition: {
-              duration: 1,
-              ease: [0.6, 0.05, -0.01, 0.9],
-            },
-          }}
-        />
+        <img src="/eye.svg" alt="" />
       </button>
     </motion.div>
   );
