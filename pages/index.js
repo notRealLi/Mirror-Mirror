d0d3d5fd3cb49d7c258af39ec0d31598c0b428bf
Li <liyunwei10@gmail.com>
2020-08-13 19:16:31 -0400

    prevent imgage from flashing on load

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,18 @@ const Index = () => {
         />
       </label>
       <button onClick={showResults} disabled={keywords === ""}>
-        <img src="/eye.svg" alt="" />
+        <motion.img
+          src="/eye.svg"
+          alt=""
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              duration: 1,
+              ease: [0.6, 0.05, -0.01, 0.9],
+            },
+          }}
+        />
       </button>
     </motion.div>
   );
