c0095aa8a63bc6c917d7a1ba2061904a85eaf5e1
Li <liyunwei10@gmail.com>
2020-08-16 12:37:58 -0400

    use useState for background particles color

--- components/background.js
+++ components/background.js
@@ -1,40 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Particles from "react-particles-js";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Background = () => {
-  const colorGold = "#e28413";
+  // const colorGold = "#e28413";
+  // const colorGreen = "#86e7b8";
+  // const colorRed = "#de3c4b";
+
+  const [color, setColor] = useState(colorRed);
+
   return (
     <div className="background">
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 15,
-              density: {
-                enable: true,
-                value_area: 800,
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          className="ease-out"
+          exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          key={Math.random()}
+        >
+          <Particles
+            params={{
+              particles: {
+                number: {
+                  value: 15,
+                  density: {
+                    enable: true,
+                    value_area: 800,
+                  },
+                },
+                color: {
+                  value: color,
+                },
+                shape: {
+                  type: "circle",
+                },
+                line_linked: {
+                  enable: false,
+                },
+                size: {
+                  value: 3,
+                  random: true,
+                  anim: {
+                    speed: 4,
+                    size_min: 0.3,
+                  },
+                },
               },
-            },
-            color: {
-              value: colorGold,
-            },
-            shape: {
-              type: "circle",
-            },
-            line_linked: {
-              enable: false,
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                speed: 4,
-                size_min: 0.3,
-              },
-            },
-          },
-        }}
-      />
+            }}
+          />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
