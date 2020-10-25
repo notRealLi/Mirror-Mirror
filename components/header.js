b12ddb845a3b00fda37b0af4c194c8e8af67a595
Li <liyunwei10@gmail.com>
2020-10-24 22:26:35 -0400

    add tubelight effect to logo

--- components/header.js
+++ components/header.js
@@ -4,9 +4,12 @@ const Header = () => {
   return (
     <div className="header">
       <div className="flex">
-        <div className="logo">
-          <a href="/">M I R R O R</a>
-        </div>
+        <a href="/">
+          <h2>
+            <span className="lazy">M</span> I R R{" "}
+            <span className="active">O</span> R
+          </h2>
+        </a>
         <div className="nav-toggle">
           <div className="nav-menu">
             <span></span>
